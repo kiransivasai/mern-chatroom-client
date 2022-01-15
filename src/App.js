7e8eef3e399c19480da7abd701ceb6dab46bd47a
kiransivasai <kiransiva2060@gmail.com>
2022-01-15 11:03:48 +0530

    Added chatroom client

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { UserContext } from "./UserContext";
+import { useEffect, useState } from "react";
+import Home from "./components/home/Home";
+import Chat from "./components/chat/Chat";
+import Navbar from "./components/layout/Navbar";
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/Signup";
 
 function App() {
+  const [user, setUser] = useState(null);
+  const menu = [
+    { title: "home", link: "/" },
+    { title: "chat", link: "/chat" },
+  ];
+  useEffect(() => {
+    const verifyUser = async () => {
+      try {
+        const res = await fetch("http://localhost:5000/verifyuser", {
+          credentials: "include",
+          headers: { "Content-Type": "application/json" },
+        });
+        const data = await res.json();
+        setUser(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    verifyUser();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <UserContext.Provider value={{ user, setUser }}>
+          <Navbar menu={menu} />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/chat/:room_id/:room_name" component={Chat} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+          </Switch>
+        </UserContext.Provider>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/chat/Chat.js
@@ -0,0 +1,62 @@
+import React, { useContext, useEffect, useState } from "react";
+import { useParams, Redirect } from "react-router-dom";
+import { UserContext } from "../../UserContext";
+import "./Chat.scss";
+import io from "socket.io-client";
+import Messages from "./messages/Messages";
+import Input from "./input/Input";
+let socket;
+
+const Chat = () => {
+  const { user } = useContext(UserContext);
+  let { room_id, room_name } = useParams();
+  const ENDPT = "localhost:5000";
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket = io(ENDPT);
+    socket.emit("join", { name: user.name, room_id, user_id: user._id });
+  }, []);
+
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
+  useEffect(() => {
+    socket.emit("get-messages-history", room_id);
+    socket.on("output-messages", (messages) => {
+      setMessages(messages);
+    });
+  }, [room_id]);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    if (message) {
+      socket.emit("send-message", message, room_id, () => {
+        setMessage("");
+      });
+    }
+  };
+  if (!user) {
+    return <Redirect to="/login" />;
+  }
+
+  return (
+    <div className="chat">
+      <div className="chat__container">
+        <h1>{room_name}</h1>
+        <Messages messages={messages} user_id={user._id} />
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
